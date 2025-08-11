6a4771fe497e89cb10c50200e7e7127c2d74c005
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-08-11 19:48:20 +0400

    added db configs

---
+++ drizzle.config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+dotenv.config({path : ".env.local"})
+
+if(!process.env.DATABASE_URL) {
+
+    throw new Error("DATABASE_URL is not set in .env.local");
+}
+
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './lib/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+
+  migrations: {
+    table: "__drizzle_migrations",
+    schema: "public",
+  },
+
+  verbose: true,
+  strict: true
+});
---
+++ lib/db/schema.ts
@@ -0,0 +1,48 @@
+import { PgTable, text, uuid, integer, boolean, pgTable , timestamp} from "drizzle-orm/pg-core";
+
+import {relations} from "drizzle-orm";
+
+export const files = pgTable("files",{
+
+    id : uuid("id").defaultRandom().primaryKey(),
+
+    //basic file/folder information
+
+    name: text("name").notNull(),
+    path: text("path").notNull(),
+    size: integer("size").notNull(),
+    type : text("type").notNull(), // folder
+
+    //storage info
+
+    fileUrl : text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_url").notNull(),
+  
+    //ownership 
+
+    userID : text("user_id").notNull(),
+    parentId : uuid("parent_id"),
+
+    //file/folder flags
+    isFolder : boolean("is_folder").default(false).notNull(),
+    isStarred : boolean("is_starred").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+    //timestamps
+
+    createdAt : timestamp("created_at").defaultNow().notNull(),
+    updatedAt : timestamp("updated_at").defaultNow().notNull()
+
+})
+
+export const filesRelations = relations(files,({one,many}) => ({
+    parent : one(files, {
+        fields:[files.parentId],
+        references: [files.id]
+    }),
+
+    children : many(files)
+}))
+
+export const File = typeof files.$inferSelect;
+export const FileInsert = typeof files.$inferInsert;
